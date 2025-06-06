64c7743b9866b5cf96c00c9b5fff526867bf06e1
raves21 <jrRanara@mcm.edu.ph>
2025-06-06 19:21:30 +0800

    chore: forgot to add route for clearOldNotifications

--- src/routes/cron.ts
+++ src/routes/cron.ts
@@ -7,4 +7,5 @@ const cronController = new CronController();
 router
   .get("/clear-unused-media", cronController.clearUnusedMedia)
   .get("/clear-expired-otcs", cronController.clearExpiredOtcs)
+  .get("/clear-old-notifications", cronController.clearOldNotifications)
 export default router;
