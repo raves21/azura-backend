ee7f601cee115daf6681112c10404ee38d4fad42
arib <johnraves23@gmail.com>
2025-12-04 13:59:39 +0800

    Update SESSION_LIMIT constant from 2 to 3

--- src/utils/constants/auth.ts
+++ src/utils/constants/auth.ts
@@ -1,4 +1,4 @@
-export const SESSION_LIMIT = 2;
+export const SESSION_LIMIT = 3;
 //1 year
 export const TOKEN_COOKIE_MAXAGE = 365 * 24 * 60 * 60 * 1000;
 export const TOKEN_EXPIRY_DATE = new Date(Date.now() + TOKEN_COOKIE_MAXAGE);
